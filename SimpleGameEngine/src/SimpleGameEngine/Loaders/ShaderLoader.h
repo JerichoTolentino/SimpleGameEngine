89b8b12834ea3bca27b36eabb1452b2b41f48408
JerichoTolentino <jerichot115@gmail.com>
2018-11-01 20:42:35 -0500

    Added doc strings to ShaderLoader methods.

--- SimpleGameEngine/src/SimpleGameEngine/Loaders/ShaderLoader.h
+++ SimpleGameEngine/src/SimpleGameEngine/Loaders/ShaderLoader.h
@@ -20,18 +20,80 @@ namespace SimpleGameEngine::Loaders
 
 
 
+		/// <summary>
+		/// Loads a shader program into memory.
+		/// </summary>
+		/// <param name="vertexFilepath">The vertex shader file.</param>
+		/// <param name="fragmentFilepath">The fragment shader file.</param>
+		/// <returns></returns>
 		static Shader loadShader(std::string vertexFilepath, std::string fragmentFilepath);
+		
+		/// <summary>
+		/// Starts a shader.
+		/// </summary>
+		/// <param name="shader"></param>
 		static void startShader(Shader shader);
+
+		/// <summary>
+		/// Stops a shader.
+		/// </summary>
+		/// <param name="shader"></param>
 		static void stopShader(Shader shader);
+		
+		/// <summary>
+		/// Deletes a shader program from memory.
+		/// </summary>
+		/// <param name="shader"></param>
 		static void destroyShader(Shader shader);
 
 
 
+		/// <summary>
+		/// Loads a float to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniform1f(Shader shader, std::string uniformName, float value);
+
+		/// <summary>
+		/// Loads an integer to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniform1i(Shader shader, std::string uniformName, int value);
+
+		/// <summary>
+		/// Loads a 2D vector to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniformVec2f(Shader shader, std::string uniformName, Vec2 value);
+		
+		/// <summary>
+		/// Loads a 3D vector to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniformVec3f(Shader shader, std::string uniformName, Vec3 value);
+		
+		/// <summary>
+		/// Loads a 4D vector to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniformVec4f(Shader shader, std::string uniformName, Vec4 value);
+		
+		/// <summary>
+		/// Loads a 4x4 matrix to a uniform variable in a shader.
+		/// </summary>
+		/// <param name="shader"></param>
+		/// <param name="uniformName"></param>
+		/// <param name="value"></param>
 		static void loadUniformMat4f(Shader shader, std::string uniformName, Mat4 value);
 	};
 }
