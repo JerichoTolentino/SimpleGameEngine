a5c9c0b3eea61b21abb223917db4c32588f0f217
JerichoTolentino <jerichot115@gmail.com>
2018-11-01 19:32:05 -0500

    Implemented Shader class.

--- SimpleGameEngine/src/SimpleGameEngine/Shaders/Shader.cpp
+++ SimpleGameEngine/src/SimpleGameEngine/Shaders/Shader.cpp
@@ -2,12 +2,31 @@
 
 namespace SimpleGameEngine::Shaders
 {
-	Shader::Shader()
+	Shader::Shader(GLuint programId, GLuint vertexShaderId, GLuint fragmentShaderId)
+	{
+		m_programId = programId;
+		m_vertexShaderId = vertexShaderId;
+		m_fragmentShaderId = fragmentShaderId;
+	}
+
+	Shader::~Shader()
 	{
 	}
 
 
-	Shader::~Shader()
+
+	GLuint Shader::getProgramId() const
 	{
+		return m_programId;
+	}
+
+	GLuint Shader::getVertexShaderId() const
+	{
+		return m_vertexShaderId;
+	}
+
+	GLuint Shader::getFragmentShaderId() const
+	{
+		return m_fragmentShaderId;
 	}
 }
--- SimpleGameEngine/src/SimpleGameEngine/Shaders/Shader.h
+++ SimpleGameEngine/src/SimpleGameEngine/Shaders/Shader.h
@@ -6,9 +6,44 @@ namespace SimpleGameEngine::Shaders
 	class Shader
 	{
 	private:
-		GLuint programID, vShaderID, fShaderID;
+		GLuint m_programId;
+		GLuint m_vertexShaderId;
+		GLuint m_fragmentShaderId;
+
+
+
 	public:
-		Shader();
+		/// <summary>
+		/// Initializes a new instance of a <see cref="Shader"/>.
+		/// </summary>
+		/// <param name="programId"></param>
+		/// <param name="vertexShaderId"></param>
+		/// <param name="fragmentShaderId"></param>
+		Shader(GLuint programId, GLuint vertexShaderId, GLuint fragmentShaderId);
+
+		/// <summary>
+		/// Destructor.
+		/// </summary>
 		virtual ~Shader();
+
+
+
+		/// <summary>
+		/// Gets the Shader Program ID.
+		/// </summary>
+		/// <returns></returns>
+		GLuint getProgramId() const;
+
+		/// <summary>
+		/// Gets the Vertex Shader ID.
+		/// </summary>
+		/// <returns></returns>
+		GLuint getVertexShaderId() const;
+
+		/// <summary>
+		/// Gets the Fragment Shader ID.
+		/// </summary>
+		/// <returns></returns>
+		GLuint getFragmentShaderId() const;
 	};
 }
