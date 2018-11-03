a2cf02cbbb2a465a1864c57bdf97706118abbf63
JerichoTolentino <jerichot115@gmail.com>
2018-11-03 15:31:16 -0500

    Import model code from previous project.

---
+++ SimpleGameEngine/src/SimpleGameEngine/Models/BoundingBox.h
@@ -0,0 +1,49 @@
+#ifndef BOUNDING_BOX_H
+#define BOUNDING_BOX_H
+
+#pragma once
+#include "../math/engineMath.h"
+#include "Camera.h"
+
+class BoundingBox
+{
+private:
+	float minx, miny, minz, maxx, maxy, maxz;
+	float mx, my, mz, mmx, mmy, mmz;	//These are the initial values that the bounding box gets when model is created
+										//DO NOT CHANGE THESE
+public:
+	BoundingBox();
+	~BoundingBox();
+
+	BoundingBox(float iminx, float iminy, float iminz, float imaxx, float imaxy, float imaxz);
+
+	//TODO: This makes this base values equal to the others transformed values!!
+	BoundingBox(const BoundingBox &other);
+	//TODO: This makes this base values equal to the others transformed values!!
+	BoundingBox& operator=(const BoundingBox &other);
+
+	int intersect(const Vec3 &ray, Camera *cam) const;
+
+	//used original values for both (so scale wouldn't be saved)
+	//void translate(const Vec3 &translate);
+	//void scale(const Vec3 &scale);
+
+	//scales first (from original vals), then translates (from scaled vals)
+	void transform(const Vec3 &scale, const Vec3 &translate);
+
+	//generate bounding box vertex array to render it on screen
+	GLfloat* generateVertices() const;
+
+	GLuint* generateIndices() const;
+
+	float getMinX() const;
+	float getMinY() const;
+	float getMinZ() const;
+	float getMaxX() const;
+	float getMaxY() const;
+	float getMaxZ() const;
+
+	std::string toString() const;
+};
+
+#endif
---
+++ SimpleGameEngine/src/SimpleGameEngine/Models/Model.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <GL/glew.h>
+#include "Material.h"
+
+class BoundingBox;	//TODO: Get lots of strange errors if we include here rather than in .cpp
+
+class Model
+{
+private:
+	GLuint vaoID;
+	int numIndices;
+	Material* material;
+	BoundingBox* baseBoundingBox;
+public:
+	Model();
+	~Model();
+
+	Model(const Model &other);
+	Model& operator=(const Model &other);
+	bool operator<(const Model &other) const;
+
+	Model(GLuint vao, int indices);
+	Model(GLuint vao, int indices, Material* mat);
+
+	Material* getMaterial() const;
+	BoundingBox* getBaseBoundingBox() const;
+	GLuint getVAO() const;
+	int getNumIndices() const;
+
+	void setMaterial(Material* m);
+	void setBaseBoundingBox(BoundingBox* box);
+
+	std::string toString() const;
+};;
