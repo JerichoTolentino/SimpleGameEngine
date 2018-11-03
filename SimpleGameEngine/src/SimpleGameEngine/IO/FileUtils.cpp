a4cf1c1e80fa3e3d3c8cb07e9e4e85b72d6d3577
JerichoTolentino <jerichot115@gmail.com>
2018-11-03 15:23:02 -0500

    Added utility method to open a file.

--- SimpleGameEngine/src/SimpleGameEngine/IO/FileUtils.cpp
+++ SimpleGameEngine/src/SimpleGameEngine/IO/FileUtils.cpp
@@ -13,13 +13,25 @@ namespace SimpleGameEngine::IO
 	{
 	}
 
-	std::string SimpleGameEngine::IO::FileUtils::loadFileToString(std::string filepath)
+	std::string FileUtils::loadFileToString(std::string filepath)
 	{
-		std::ifstream ifs(filepath);
+		std::ifstream ifs = openFile(filepath);
 		std::string content(
 			(std::istreambuf_iterator<char>(ifs)), 
 			(std::istreambuf_iterator<char>()));
 
 		return content;
 	}
+
+	std::ifstream FileUtils::openFile(std::string filepath)
+	{
+		std::ifstream ifs;
+		
+		ifs.open(filepath.c_str());
+		if (!ifs.is_open())
+			throw IOException("Failed to open file " + filepath);
+
+		return ifs;
+	}
+
 }
--- SimpleGameEngine/src/SimpleGameEngine/IO/FileUtils.h
+++ SimpleGameEngine/src/SimpleGameEngine/IO/FileUtils.h
@@ -15,6 +15,15 @@ namespace SimpleGameEngine::IO
 		/// </summary>
 		/// <param name="filepath">The path to the file to read.</param>
 		/// <returns></returns>
+		/// <exception cref="IOException"></exception>
 		static std::string loadFileToString(std::string filepath);
+
+		/// <summary>
+		/// Opens a file.
+		/// </summary>
+		/// <param name="filepath"></param>
+		/// <returns></returns>
+		/// <exception cref="IOException"></exception>
+		static std::ifstream openFile(std::string filepath);
 	};
 }
