733a2a7d4fd843ee4b8597e48d9beef5b619ce44
JerichoTolentino <jerichot115@gmail.com>
2018-11-03 15:56:34 -0500

    Cleaned up Model class.

--- SimpleGameEngine/src/SimpleGameEngine/Models/Model.cpp
+++ SimpleGameEngine/src/SimpleGameEngine/Models/Model.cpp
@@ -1,97 +1,98 @@
 #include "Model.h"
-#include "BoundingBox.h"
 
-
-Model::Model()
+namespace SimpleGameEngine::Models
 {
-	vaoID = -1;
-	numIndices = 0;
-	material = new Material;
-	baseBoundingBox = new BoundingBox;
-}
+	Model::Model()
+	{
+		vaoID = -1;
+		numIndices = 0;
+		material = new Material;
+		baseBoundingBox = new BoundingBox;
+	}
 
 
-Model::~Model()
-{
-	delete baseBoundingBox;
-}
+	Model::~Model()
+	{
+		delete baseBoundingBox;
+	}
 
-Model::Model(const Model &other)
-{
-	vaoID = other.vaoID;
-	numIndices = other.numIndices;
-	material = new Material(*(other.material));
-	baseBoundingBox = new BoundingBox(*(other.getBaseBoundingBox()));
-}
+	Model::Model(const Model &other)
+	{
+		vaoID = other.vaoID;
+		numIndices = other.numIndices;
+		material = new Material(*(other.material));
+		baseBoundingBox = new BoundingBox(*(other.getBaseBoundingBox()));
+	}
 
-Model& Model::operator=(const Model &other)
-{
-	vaoID = other.vaoID;
-	numIndices = other.numIndices;
-	*material = *(other.material);
-	*baseBoundingBox = *(other.getBaseBoundingBox());
-	return *this;
-}
+	Model& Model::operator=(const Model &other)
+	{
+		vaoID = other.vaoID;
+		numIndices = other.numIndices;
+		*material = *(other.material);
+		*baseBoundingBox = *(other.getBaseBoundingBox());
+		return *this;
+	}
 
-bool Model::operator<(const Model & other) const
-{
-	bool less = false;
+	bool Model::operator<(const Model & other) const
+	{
+		bool less = false;
 
-	if (other.getVAO() < vaoID)
-		less = true;
+		if (other.getVAO() < vaoID)
+			less = true;
 
-	return less;
-}
+		return less;
+	}
 
-Model::Model(GLuint vao, int indices)
-{
-	vaoID = vao;
-	numIndices = indices;
-	material = new Material;
-}
+	Model::Model(GLuint vao, int indices)
+	{
+		vaoID = vao;
+		numIndices = indices;
+		material = new Material;
+	}
 
-Model::Model(GLuint vao, int indices, Material* mat)
-{
-	vaoID = vao;
-	numIndices = indices;
-	material = mat;
-}
+	Model::Model(GLuint vao, int indices, Material* mat)
+	{
+		vaoID = vao;
+		numIndices = indices;
+		material = mat;
+	}
 
-Material* Model::getMaterial() const
-{
-	return material;
-}
-
-
-BoundingBox * Model::getBaseBoundingBox() const
-{
-	return baseBoundingBox;
-}
+	Material* Model::getMaterial() const
+	{
+		return material;
+	}
 
 
-GLuint Model::getVAO() const
-{
-	return vaoID;
-}
-
-int Model::getNumIndices() const
-{
-	return numIndices;
-}
-
-void Model::setMaterial(Material* m)
-{
-	material = m;
-}
+	BoundingBox * Model::getBaseBoundingBox() const
+	{
+		return baseBoundingBox;
+	}
 
 
-void Model::setBaseBoundingBox(BoundingBox * box)
-{
-	this->baseBoundingBox = box;
-}
+	GLuint Model::getVAO() const
+	{
+		return vaoID;
+	}
+
+	int Model::getNumIndices() const
+	{
+		return numIndices;
+	}
+
+	void Model::setMaterial(Material* m)
+	{
+		material = m;
+	}
 
 
-std::string Model::toString() const
-{
-	return ("--Model--\nVaoID: " + std::to_string(vaoID) + "\nnumIndices: " + std::to_string(numIndices) + "\n" + material->toString() + "\n--End Model--");
+	void Model::setBaseBoundingBox(BoundingBox * box)
+	{
+		this->baseBoundingBox = box;
+	}
+
+
+	std::string Model::toString() const
+	{
+		return ("--Model--\nVaoID: " + std::to_string(vaoID) + "\nnumIndices: " + std::to_string(numIndices) + "\n" + material->toString() + "\n--End Model--");
+	}
 }
--- SimpleGameEngine/src/SimpleGameEngine/Models/Model.h
+++ SimpleGameEngine/src/SimpleGameEngine/Models/Model.h
@@ -1,34 +1,36 @@
 #pragma once
 #include <GL/glew.h>
+#include "BoundingBox.h"
 #include "Material.h"
 
-class BoundingBox;	//TODO: Get lots of strange errors if we include here rather than in .cpp
-
-class Model
+namespace SimpleGameEngine::Models
 {
-private:
-	GLuint vaoID;
-	int numIndices;
-	Material* material;
-	BoundingBox* baseBoundingBox;
-public:
-	Model();
-	~Model();
+	class Model
+	{
+	private:
+		GLuint vaoID;
+		int numIndices;
+		Material* material;
+		BoundingBox* baseBoundingBox;
+	public:
+		Model();
+		~Model();
 
-	Model(const Model &other);
-	Model& operator=(const Model &other);
-	bool operator<(const Model &other) const;
+		Model(const Model &other);
+		Model& operator=(const Model &other);
+		bool operator<(const Model &other) const;
 
-	Model(GLuint vao, int indices);
-	Model(GLuint vao, int indices, Material* mat);
+		Model(GLuint vao, int indices);
+		Model(GLuint vao, int indices, Material* mat);
 
-	Material* getMaterial() const;
-	BoundingBox* getBaseBoundingBox() const;
-	GLuint getVAO() const;
-	int getNumIndices() const;
+		Material* getMaterial() const;
+		BoundingBox* getBaseBoundingBox() const;
+		GLuint getVAO() const;
+		int getNumIndices() const;
 
-	void setMaterial(Material* m);
-	void setBaseBoundingBox(BoundingBox* box);
+		void setMaterial(Material* m);
+		void setBaseBoundingBox(BoundingBox* box);
 
-	std::string toString() const;
-};;
+		std::string toString() const;
+	};
+}
