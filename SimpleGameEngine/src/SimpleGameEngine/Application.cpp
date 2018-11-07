254f861b5484e64dd5067f8ff60bf041c6b2d068
JerichoTolentino <jerichot115@gmail.com>
2018-11-06 19:27:19 -0600

    Added temporary test code to Application.cpp.

--- SimpleGameEngine/src/SimpleGameEngine/Application.cpp
+++ SimpleGameEngine/src/SimpleGameEngine/Application.cpp
@@ -1,7 +1,13 @@
 #include "Application.h"
 #include "Display/Window.h"
+#include "Parsers/WavefrontObjParser.h"
+#include "Parsers/ParseException.h"
+#include "Models/GeometryModel.h"
+#include "Log.h"
 
 using namespace SimpleGameEngine::Display;
+using namespace SimpleGameEngine::Parsers;
+using namespace SimpleGameEngine::Models;
 
 namespace SimpleGameEngine
 {
@@ -16,6 +22,16 @@ namespace SimpleGameEngine
 
 	void Application::Run()
 	{
+		try
+		{
+			GeometryModel stallModel = WavefrontObjParser::parseFile("D:/Blender Files/stall.obj");
+			SGE_CORE_WARNING("Successfully loaded stall model.");
+		}
+		catch (ParseException e)
+		{
+			SGE_ERROR("Failed to load stall model." + std::string(e.what()));
+		}
+
 		Window window;
 		while (true)
 		{
