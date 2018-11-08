05ab3e8103c73c132990b992a229ad17d50efb71
JerichoTolentino <jerichot115@gmail.com>
2018-11-07 22:02:40 -0600

    Imported/cleaned up Camera class from previous project.

---
+++ SimpleGameEngine/src/SimpleGameEngine/Cameras/Camera.cpp
@@ -0,0 +1,197 @@
+#include "Camera.h"
+
+#define MOVESPEED 10.0f
+#define ROTSPEED 40.0f
+#define DEFAULT_GROUND_ANGLE 30.0f
+#define DEFAULT_DISTANCE 5.0f
+#define DEFAULT_AROUND_ANGLE 0.0f
+
+using namespace SimpleGameEngine::Display;
+using namespace SimpleGameEngine::Math;
+using namespace SimpleGameEngine::Models;
+
+namespace SimpleGameEngine::Cameras
+{
+	Camera::Camera()
+	{
+		position = Vec3(0, 0, 0);
+		rotation = Vec3(0, 0, 0);
+		groundAngle = DEFAULT_GROUND_ANGLE;
+		aroundAngle = DEFAULT_AROUND_ANGLE;
+		distanceFromEntity = DEFAULT_DISTANCE;
+	}
+
+	Camera::Camera(Vec3 pos, Vec3 rot)
+	{
+		position = pos;
+		rotation = rot;
+		groundAngle = DEFAULT_GROUND_ANGLE;
+		aroundAngle = DEFAULT_AROUND_ANGLE;
+		distanceFromEntity = DEFAULT_DISTANCE;
+	}
+
+	Camera::~Camera()
+	{
+	}
+
+
+
+	Vec3 Camera::getPosition() const
+	{
+		return position;
+	}
+
+	Vec3 Camera::getRotation() const
+	{
+		return rotation;
+	}
+
+	float Camera::getGroundAngle() const
+	{
+		return groundAngle;
+	}
+
+	float Camera::getAroundAngle() const
+	{
+		return aroundAngle;
+	}
+
+	float Camera::getDistanceFromEntity() const
+	{
+		return distanceFromEntity;
+	}
+
+	bool Camera::isLockedHorizontal() const
+	{
+		return lockedHorizontal;
+	}
+
+	bool Camera::isLockedVertical() const
+	{
+		return lockedVertical;
+	}
+
+	Mat4 Camera::generateViewMatrix() const
+	{
+		return Mat4::generateViewMatrix(position, rotation);
+	}
+
+	Mat4 Camera::generateSkyboxViewMatrix() const
+	{
+		Mat4 view = Mat4::generateViewMatrix(position, rotation);
+
+		// Remove the translation part of the matrix (only want to rotate the skybox)
+		view.elements[12] = 0;
+		view.elements[13] = 0;
+		view.elements[14] = 0;
+		
+		return view;
+	}
+
+	void Camera::setPosition(Vec3 pos)
+	{
+		position = pos;
+	}
+
+	void Camera::setRotation(Vec3 rot)
+	{
+		rotation = rot;
+	}
+
+	void Camera::setGroundAngle(float angle)
+	{
+		groundAngle = angle;
+	}
+
+	void Camera::setAroundAngle(float angle)
+	{
+		aroundAngle = angle;
+	}
+
+	void Camera::setDistanceFromEntity(float distance)
+	{
+		distanceFromEntity = distance;
+	}
+
+	void Camera::setLockedHorizontal(bool locked)
+	{
+		lockedHorizontal = locked;
+	}
+
+	void Camera::setLockedVertical(bool locked)
+	{
+		lockedVertical = locked;
+	}
+
+	void Camera::moveCamera(Window window)
+	{
+		double mx, my;
+		float timeFactor = window.getTimeFactor();
+		window.getDeltaMousePos(mx, my);
+
+		rotation += Vec3(ROTSPEED * my * timeFactor, ROTSPEED * mx * timeFactor, 0);
+
+		if (window.isKeyDown(GLFW_KEY_PAGE_UP))
+		{
+			position += Vec3(0, MOVESPEED * timeFactor, 0);
+		}
+		if (window.isKeyDown(GLFW_KEY_PAGE_DOWN))
+		{
+			position -= Vec3(0, MOVESPEED * timeFactor, 0);
+		}
+		if (window.isKeyDown(GLFW_KEY_D))
+		{
+			float zFactor = cos(MathUtils::toRadians(rotation.y));
+			float xFactor = sin(MathUtils::toRadians(rotation.y));
+			position += Vec3(zFactor * MOVESPEED * timeFactor, 0, xFactor * MOVESPEED * timeFactor);
+		}
+		if (window.isKeyDown(GLFW_KEY_A))
+		{
+			float zFactor = cos(MathUtils::toRadians(rotation.y));
+			float xFactor = sin(MathUtils::toRadians(rotation.y));
+			position -= Vec3(zFactor * MOVESPEED * timeFactor, 0, xFactor * MOVESPEED * timeFactor);
+		}
+		if (window.isKeyDown(GLFW_KEY_W))
+		{
+			float zFactor = cos(MathUtils::toRadians(rotation.y));
+			float xFactor = -sin(MathUtils::toRadians(rotation.y));
+			position -= Vec3(MOVESPEED * xFactor * timeFactor, 0, MOVESPEED * zFactor * timeFactor);
+		}
+		if (window.isKeyDown(GLFW_KEY_S))
+		{
+			float zFactor = cos(MathUtils::toRadians(rotation.y));
+			float xFactor = -sin(MathUtils::toRadians(rotation.y));
+			position += Vec3(MOVESPEED * xFactor * timeFactor, 0, MOVESPEED * zFactor * timeFactor);
+		}
+	}
+
+	void Camera::follow(SpaceModel spaceModel)
+	{
+		position = spaceModel.getPosition();
+		rotation.x = spaceModel.getRotation().x + groundAngle;
+		rotation.y = aroundAngle;
+
+		float theta = MathUtils::toRadians(groundAngle);
+		float zFactor = cos(MathUtils::toRadians(rotation.y));
+		float xFactor = -sin(MathUtils::toRadians(rotation.y));
+		
+		position.x += cos(theta) * distanceFromEntity * xFactor;
+		position.z += cos(theta) * distanceFromEntity * zFactor;
+		position.y += sin(theta) * distanceFromEntity;
+	}
+
+	void Camera::zoom(float distance, float timeFactor)
+	{
+		distanceFromEntity += distance * timeFactor;
+	}
+
+	void Camera::rotateVertical(float angle, float timeFactor)
+	{
+		groundAngle += angle * timeFactor;
+	}
+
+	void Camera::rotateAround(float angle, float timeFactor)
+	{
+		aroundAngle += angle * timeFactor;
+	}
+}
---
+++ SimpleGameEngine/src/SimpleGameEngine/Cameras/Camera.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "../Display/Window.h"
+#include "../Math/Mat4.h"
+#include "../Math/Vec3.h"
+#include "../Models/SpaceModel.h"
+
+namespace SimpleGameEngine::Cameras
+{
+	class Camera
+	{
+	private:
+		Math::Vec3 position;
+		Math::Vec3 rotation;
+		float groundAngle;
+		float aroundAngle;
+		float distanceFromEntity;
+		bool lockedHorizontal;
+		bool lockedVertical;
+
+
+
+	public:
+		Camera();
+		Camera(Math::Vec3 pos, Math::Vec3 rot);
+		~Camera();
+
+
+		Math::Vec3 getPosition() const;
+		Math::Vec3 getRotation() const;
+		float getGroundAngle() const;
+		float getAroundAngle() const;
+		float getDistanceFromEntity() const;
+		bool isLockedHorizontal() const;
+		bool isLockedVertical() const;
+		Math::Mat4 generateViewMatrix() const;
+		Math::Mat4 generateSkyboxViewMatrix() const;
+
+		void setPosition(Math::Vec3 pos);
+		void setRotation(Math::Vec3 rot);
+		void setGroundAngle(float angle);
+		void setAroundAngle(float angle);
+		void setDistanceFromEntity(float distance);
+		void setLockedHorizontal(bool locked);
+		void setLockedVertical(bool locked);
+
+		void moveCamera(Display::Window window);
+		void follow(Models::SpaceModel spaceModel);
+		void zoom(float distance, float timeFactor);
+		void rotateVertical(float angle, float timeFactor);
+		void rotateAround(float angle, float timeFactor);
+	};
+}
