e42dbab562d931a169ec824025ac4ea2d949caf3
JerichoTolentino <jerichot115@gmail.com>
2018-11-14 20:45:54 -0600

    Fixed syntax error in Log.h with log level fatal to critical.

--- SimpleGameEngine/src/SimpleGameEngine/Log.h
+++ SimpleGameEngine/src/SimpleGameEngine/Log.h
@@ -24,7 +24,7 @@ namespace SimpleGameEngine
 }
 
 // Core log macros
-#define SGE_CORE_FATAL(...)		::SimpleGameEngine::Log::getCoreLogger()->fatal(__VA_ARGS__)
+#define SGE_CORE_FATAL(...)		::SimpleGameEngine::Log::getCoreLogger()->critical(__VA_ARGS__)
 #define SGE_CORE_ERROR(...)		::SimpleGameEngine::Log::getCoreLogger()->error(__VA_ARGS__)
 #define SGE_CORE_WARNING(...)	::SimpleGameEngine::Log::getCoreLogger()->warn(__VA_ARGS__)
 #define SGE_CORE_INFO(...)		::SimpleGameEngine::Log::getCoreLogger()->info(__VA_ARGS__)
@@ -32,7 +32,7 @@ namespace SimpleGameEngine
 #define SGE_CORE_DEBUG(...)		::SimpleGameEngine::Log::getCoreLogger()->debug(__VA_ARGS__)
 
 // Client log macros
-#define SGE_FATAL(...)		::SimpleGameEngine::Log::getClientLogger()->fatal(__VA_ARGS__)
+#define SGE_FATAL(...)		::SimpleGameEngine::Log::getClientLogger()->critical(__VA_ARGS__)
 #define SGE_ERROR(...)		::SimpleGameEngine::Log::getClientLogger()->error(__VA_ARGS__)
 #define SGE_WARNING(...)	::SimpleGameEngine::Log::getClientLogger()->warn(__VA_ARGS__)
 #define SGE_INFO(...)		::SimpleGameEngine::Log::getClientLogger()->info(__VA_ARGS__)
